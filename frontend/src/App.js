ff924b98a138979ba3a8cf24e650616e62d936bd
thrupthi2002 <96109514+thrupthi2002@users.noreply.github.com>
2022-10-25 12:33:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React from "react";
+import Form from "./login_signup";
+import{
+    BrowserRouter as Router,
+    Route,
+    Routes
+  }from "react-router-dom";
+import Home from "./home";
+class App extends React.Component{
+    render(){
+    return(
+    <Router>
+        <Routes>
+            <Route  path="/home" element={<Home/>}></Route>
+            <Route path="/" element={<Form/>}></Route>
+        </Routes>
+    </Router>
+    
+    )
+    }
+};
 export default App;
