830c80323d8076a5da726009f7402291c581d23c
thrupthi2002 <96109514+thrupthi2002@users.noreply.github.com>
2022-11-13 18:27:06 +0530

    update popup.js

--- frontend/src/components/popup.js
+++ frontend/src/components/popup.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import Popup from 'reactjs-popup';
+import React, { useState } from 'react';
+//import Popup from 'reactjs-popup';
 import './popup_style.css';
 
 function Upload({isshow1}) {
 
   console.log(isshow1);
+  const [file, setFile] = useState();
+  function handleChange(e) {
+      console.log(e.target.files);
+      setFile(URL.createObjectURL(e.target.files[0]));
+      //<label style={{color:"white"}}>Remove Art: </label><br/>
+      //<button onClick={removeimg}>remove</button><br/><br/>
+  }
+
+function removeimg(e){
+  setFile();
+  e.preventDefault();
+}
+
   return (
     <div className={`${!isshow1 ? "outer": ""} show`}>
     <div className="form">
     <div className="form-box solid">
-      <form>
-        <label>Title: </label><br/><br/>
+      <form style={{display:"flex",flexDirection:"row",boxSizing: "border-box"}}>
+        <div>
+        <label style={{color:"white"}}>Title: </label><br/>
         <input
         type="text"
-        name="title" />
-      </form>
+        name="title" /><br/><br/>
 
-      <label>Artist Name: </label><br/><br/>
-      <input type="text" name="artist" />
-      <label>Upload your work: </label><br/><br/>
-      <input type="file" name="art" />
-      <img id="myart" src="#" alt="My image" />
-      <input type="submit"/>
+      <label style={{color:"white"}}>Artist Name: </label><br/>
+      <input type="text" name="artist" /><br/><br/>
+      <label style={{color:"white"}}>Upload your work: </label><br/>
+      <input type="file" name="art" onChange={handleChange} /><br/><br/>
+      <input type="submit"  style={{width:"100px"}}/>
+      </div>
+      <div style={{width:"250px",height:"250px",backgroundColor:"white",display:"flex",justifyContent:"center"}}>
+      <div ><img id="myart" src={file}  width="100%" height="100%" alt="upload your artwork!" /></div><br/><br/>
+      </div>
+      </form>
     </div>
     </div>
     </div>
