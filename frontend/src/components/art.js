eafadbb0491138494874628e6069cb05ad1f4bcb
thrupthi2002 <96109514+thrupthi2002@users.noreply.github.com>
2022-11-16 23:22:23 +0530

    final commit

--- frontend/src/components/art.js
+++ frontend/src/components/art.js
@@ -1,23 +1,99 @@
-import React from 'react';
-import Upload from './popup';
 
-function Art(props) {
+import axios from 'axios';
+import React,{useEffect} from 'react';
+import {useNavigate, useLocation, useParams} from 'react-router-dom';
+import { useState } from 'react';
 
+//import Upload from './popup';
+
+
+function Art({image,name,desc,type,user}) {
+  //const location = useLocation();
+  const [disp, setdisp] = useState('none');
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(type=='artist')
+    {
+      console.log('artist');
+      setdisp('block');
+    }
+    else
+    {
+      console.log('home');
+      setdisp('none');
+    }
+    
+ });
+/*  const Check=() =>{
+    if(props.type=='artist')
+    {
+      console.log('artist');
+      setdisp('block');
+    }
+    else
+    {
+      console.log('home');
+      setdisp('none');
+    }*/
+    //<label style={{color:"white"}}>Remove Art: </label><br/>
+    //<button onClick={removeimg}>remove</button><br/><br/>
+    const deleteart = (e) => {
+      
+      //confirm(true);
+      if(window.confirm("do you really want to delete the art??")==true)
+      {
+      e.preventDefault();
+      axios.delete(
+        `http://localhost:8000/delete_art/${desc}/${name}`
+    
+       ).then((response)=>{
+         //console.log(response)
+         console.log(response.status)
+         alert('art deleted!')
+         e.preventDefault();
+         console.log("hello")
+         console.log(user)
+          navigate("/artist/home",{state:{user}});
+    
+       }).catch((err)=>{
+        console.log("Nooo")
+      })
+      e.preventDefault();
+    }
+    }
+      
   const handleClick = () => {
-    props.handlePopup2Open()
-  }
+    
+    axios.get(
+      `http://localhost:8000/get_artwork_desc/${desc}/${name}`
+  
+     ).then((response)=>{
+       //console.log(response.data)
+       //console.log(response.status)
+       const items = (response.data)[0];
+       //console.log(items)
+       navigate("/art",{state:{items}})
+  
+  
+     }).catch((err)=>{
+      console.log("Nooo")
+    })
+  };
+  
+  
   var divpic = {
     display: 'inline-block',
-    height: 50,
-    paddingLeft: 50,
+    height: '140px',
+    paddingLeft: '150px',
+    paddingBottom:"100px",
     justifyContent: 'center'
   }
   return (
-    <div style={divpic}>
-      <img src={props.image} onClick={handleClick} />
-      <p><b>{props.name}</b></p>
-      <p><em>{props.desc}</em></p>
-      <Upload isshow2={props.isshow2} />
+    <div  style={divpic}>
+      <img className='art' src={image} width='100%' height='100%' onClick={handleClick} />
+      <p style={{position:"relative",top:"0",display:disp,margin:"auto"}}><button style={{width:"100%",fontSize:"25px"}} onClick={deleteart}>-</button></p>
+      <p><b>{name}</b></p>
+      <p><em>{desc}</em></p>
     </div>
 
   );
