e7b996bb57af9c8646e227a7e225f51336ec1944
thrupthi2002 <96109514+thrupthi2002@users.noreply.github.com>
2022-11-16 23:26:38 +0530

    final commit

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,5 @@
-from django.contrib.postgres.fields import ArrayField
 from django.db import models
+#from django.contrib.postgres.fields import ArrayField
 from datetime import date
 
 # Create your models here.
